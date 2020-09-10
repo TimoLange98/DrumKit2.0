8c1a3d23a11025c2a67371407bc695fbfcd5a6c1
TimoL <timo.lange98@aol.de>
2020-09-10 23:54:03 +0200

    First design, working on waiting for timeout function for displaying the steps in the sequencer

--- code.js
+++ code.js
@@ -1,8 +1,28 @@
 const bpmSlider = document.getElementById('bpm-slider');
 const bpmDisplay = document.getElementById('bpm-display');
 
-var bpm = 120;
+const steps = Array.from(document.getElementById('seq-steps-container').children);
+
+var bpm;
+var playing = false;
 
 const showVal = (val) => {
     bpmDisplay.textContent = `${val} bpm`;
+    bpm = val; 
+}
+
+const play = async () => {
+    steps.forEach(step => {
+        await blink(step);
+    })
+}
+
+const blink = () => {
+    return new Promise(resolve => {
+        setTimeout(resolve, 1000);
+    })
+}
+
+const stop = () => {
+    playing = false;
 }
